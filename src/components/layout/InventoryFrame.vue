e262087d5315cc8aba6dc463147b93ca532b0223
Jaffies <untitledjaffies@gmail.com>
2025-02-01 19:54:13 +0500

    Some first setup

---
+++ src/components/layout/InventoryFrame.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<template>
+    <div class="background">
+        <div class="">
+            <span>asdf</span>
+            <span>asdfasdf</span>
+            <!-- <LeftGhostContainer />
+            <InventoryContainer /> -->
+        </div>
+
+        <BottomGhostContainer />
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .background {
+        display: flex;
+        padding: 32px;
+        width: 640px;
+        height: 480px;
+        background-color: $background-frame-color;
+    }
+
+    .flex {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        width: 100%
+    }
+</style>
---
+++ src/components/layout/LeftGhostContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="background">
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .background {
+        width: 10px;
+        height: 10px;
+        border-radius: $border-radius;
+        border: $border-width solid $border-color;
+    }
+</style>
