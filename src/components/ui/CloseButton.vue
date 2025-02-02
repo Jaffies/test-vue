db22f5dfc9bfe5f5e981acac591056044dbc029b
Jaffies <untitledjaffies@gmail.com>
2025-02-02 18:02:17 +0500

    Better theming

--- src/components/ui/CloseButton.vue
+++ src/components/ui/CloseButton.vue
@@ -11,14 +11,17 @@ const emit = defineEmits<{
 </template>
 
 <style lang="scss" scoped>
-@use "/settings.scss" as *;
 .close-button {
     position: absolute;
     right: 8px;
     top: 8px;
     width: 14px;
     height: 14px;
+}
 
-    filter: if($light, invert(100%), none);
+@media (prefers-color-scheme: light) {
+    .close-button {
+        filter : invert(100%)
+    }
 }
 </style>
